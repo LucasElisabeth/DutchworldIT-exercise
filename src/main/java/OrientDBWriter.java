568a8aeeecfa35a1c03c5b832856ecea3aa17300
LucasElisabeth <LpsElisabeth2001@gmail.com>
2020-02-26 02:02:57 +0100

    added some checks to WriteToDB method, now checks if age is a double and if the date is properly formatted.

--- src/main/java/OrientDBWriter.java
+++ src/main/java/OrientDBWriter.java
@@ -6,11 +6,15 @@ import com.orientechnologies.orient.core.sql.executor.OResult;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OrientDBWriter {
 
     private String url;
+    private SimpleDateFormat format;
     private File csvFile;
     private CSVReader reader = new CSVReader();
     private OrientDB orientDB;
@@ -21,6 +25,7 @@ public class OrientDBWriter {
     public OrientDBWriter(File file, String url) {
         csvFile = file;
         this.url = url;
+        format = new SimpleDateFormat("dd-MM-yyyy");
     }
 
     public void connectToDB(String DBName) {
@@ -44,18 +49,27 @@ public class OrientDBWriter {
             String[] strings = reader.translateLine(string);
             ODocument doc = new ODocument("Person");
 
-            doc.field("name", strings[0]);
-            doc.field("workplace", strings[1]);
-            doc.field("date", strings[2]);
-            doc.field("hometown", strings[3]);
-            doc.field("age", strings[4]);
-            doc.field("description", strings[5]);
+            try {
+                doc.field("date", format.parse(strings[2]));
+                doc.field("age", Double.parseDouble(strings[4]));
+                doc.field("name", strings[0]);
+                doc.field("workplace", strings[1]);
+                doc.field("hometown", strings[3]);
+                doc.field("description", strings[5]);
 
+            } catch (ParseException ex) {
+                System.err.println("The date format is incorrect. Please use \"dd/MM/yyyy\"");
+                continue;
+            } catch (NumberFormatException ex) {
+                System.err.println("This is not a proper age. Please use numbers.");
+                continue;
+            }
             db.save(doc);
         }
 
         System.out.println(OPERATION_COMPLETE_MESSAGE);
     }
+
 
     public void removeFromDB(ODatabaseSession db) {
 
@@ -84,6 +98,37 @@ public class OrientDBWriter {
         System.out.println(OPERATION_COMPLETE_MESSAGE);
     }
 
+    public void addNewRecord(ODatabaseSession db, Scanner scanner) {
+
+        try {
+
+            ODocument doc = new ODocument("Person");
+
+            System.out.println("date:");
+            doc.field("date", format.parse(scanner.nextLine()));
+            System.out.println("Age:");
+            doc.field("age:", Double.parseDouble(scanner.nextLine()));
+            System.out.println("Name:");
+            doc.field("name", scanner.nextLine());
+            System.out.println("workplace:");
+            doc.field("workplace", scanner.nextLine());
+            System.out.println("hometown:");
+            doc.field("hometown", scanner.nextLine());
+            System.out.println("description:");
+            doc.field("description:", scanner.nextLine());
+
+            db.save(doc);
+
+        } catch (ParseException ex) {
+            System.err.println("The date format is incorrect. Please use \"dd/MM/yyyy\"");
+            closeConnection(db, orientDB);
+            System.exit(1);
+        } catch (NumberFormatException ex) {
+            System.err.println("This is not a proper age. Please use numbers.");
+            closeConnection(db, orientDB);
+            System.exit(1);
+        }
+    }
 
     public String getUrl() {
         return url;
